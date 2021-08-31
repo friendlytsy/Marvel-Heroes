2aa097b61ff10bc1c62d95abcb6e7641dfac7348
AT <n/a>
2021-08-31 11:32:36 +0300

    Initial Commit

---
+++ Marvel Heroes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marvel Heroes
+//
+//  Created by Александр Цыганков on 31.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
